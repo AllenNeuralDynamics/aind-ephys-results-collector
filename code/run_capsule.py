8fde5089b3245aacce4552d8f60ae9482bb83daa
Alessio Buccino <alessio.buccino@alleninstitute.org>
2024-04-10 07:18:43 +0000

    Use AI instead of AIND as funder

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -230,7 +230,7 @@ if __name__ == "__main__":
         data_description_dict["institution"] = Organization.AIND
         data_description_dict["data_level"] = DataLevel.RAW
         data_description_dict["investigators"] = ["Unknown"]
-        data_description_dict["funding_source"] = [Funding(funder=Organization.AIND)]
+        data_description_dict["funding_source"] = [Funding(funder=Organization.AI)]
         data_description_dict["modality"] = [Modality.ECEPHYS]
         data_description_dict["platform"] = Platform.ECEPHYS
         data_description_dict["subject_id"] = subject_id
