e4d8821b7585a96314e2a1323ff22b5e9aef0158
Alessio Buccino <alejoe9187@gmail.com>
2026-09-11 10:59:48 +0200

    fix: clean up partial output folder in case of spike sorting failure (#23)

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -49,7 +49,6 @@ try:
 except ImportError:
     HAVE_AIND_LOG_UTILS = False
 
-PIPELINE_MAINAINER = "Alessio Buccino"
 PIPELINE_NAME = "AIND Ephys Pipeline"
 PIPELINE_URL = os.getenv("PIPELINE_URL", "")
 PIPELINE_VERSION = os.getenv("PIPELINE_VERSION", "")
@@ -325,6 +324,9 @@ if __name__ == "__main__":
             analyzer = si.load(analyzer_output_folder, load_extensions=False)
         except:
             logging.info(f"\t\tSpike sorting failed on {recording_name}. Skipping collection")
+            # Clean up any partially copied results
+            if analyzer_output_folder.is_dir():
+                shutil.rmtree(analyzer_output_folder)
             continue
 
         # add labels
